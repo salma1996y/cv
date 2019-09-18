e37710ff84050f0e579f967f66bb7e47efee0256
salma1996y <55484382+salma1996y@users.noreply.github.com>
2019-09-18 12:05:15 +0300

    Create cv.js

---
+++ cv.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <title>
+            cv salma aljaradah
+        </title>
+         
+    <link  href="cv.css" rel="stylesheet"/>
+ 
+              
+    </head>
+
+    <body dir="rtl">
+<div class="all"> 
+  
+   <div id="cv" class="salma" class="pa">
+   <p>  <i class="fas fa-angle-left" ></i>
+     <a href="https://drive.google.com/file/d/0B09jCzglpRQmaFJQRkh6SUlaU2pKRXRMeDh2ZkJRM0xYb0Rj/view?usp=sharing"> CV </a>
+     <i class="fas fa-angle-right"></i> </p>
+    </div>
+  
+    <div class="top" id="right">
+    <h3> Salma </h3>
+  </div>
+  <div class="top" id="left">
+    <h1>Ibrahim</h1>
+  </div>
+  </div>
+                 <script src="cv.js">
+        </script>
+    </body>
+</html>
